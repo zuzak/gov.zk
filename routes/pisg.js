5637e3db1ba7bd5dc1a30c55ae36db9d531940ca
Douglas Gardner <douglas@chippy.ch>
2019-05-01 20:20:25 +0000

    Handle missing pisg output better

--- routes/pisg.js
+++ routes/pisg.js
@@ -3,9 +3,15 @@ const path = require('path')
 const fs = require('fs')
 
 app.get(__l('/chan-stats'), function (req, res, next) {
-  let data = fs.readFileSync(path.resolve(path.join(__dirname, '..', 'pisg', 'index.html')), 'utf-8')
-  let body = data.substring(data.lastIndexOf('<body>') + 6, data.lastIndexOf('</body>'))
-  let css = data.substring(data.lastIndexOf('<style>') + 8, data.lastIndexOf('</style>'))
+  try {
+    let data = fs.readFileSync(path.resolve(path.join(__dirname, '..', 'pisg', 'index.html')), 'utf-8')
+    let body = data.substring(data.lastIndexOf('<body>') + 6, data.lastIndexOf('</body>'))
+    let css = data.substring(data.lastIndexOf('<style>') + 8, data.lastIndexOf('</style>'))
 
-  return res.render('pisg.pug', {pisg: body, req, style: css})
+    return res.render('pisg.pug', {pisg: body, req, style: css})
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return next()
+    }
+  }
 })
