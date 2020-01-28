5269dd01661c4410f24461324951934696c2cade
Douglas Gardner <douglas@chippy.ch>
2020-01-28 15:50:05 +0000

    Remove hyphen from civil-servant

--- routes/civilservant.js
+++ routes/civilservant.js
@@ -1,12 +1,12 @@
 var app = require('..')
-let htmlEscape = require('html-escape')
-let AU = require('ansi_up')
-let request = require('request-promise')
+const htmlEscape = require('html-escape')
+const AU = require('ansi_up')
+const request = require('request-promise')
 
-let scram = "ok"
+let scram = 'ok'
 
-app.get(__l('/civil-servant'), async function (req, res, next) {
-  let [inventory, ofn, quotes, scrabble] = await Promise.all([
+app.get(__l('/civilservant'), async function (req, res, next) {
+  const [inventory, ofn, quotes, scrabble] = await Promise.all([
     request({
       url: 'https://files.chippy.ch/cs/inventory.json',
       json: true
@@ -35,9 +35,9 @@ app.get(__l('/civil-servant'), async function (req, res, next) {
   })
 })
 
-app.get('/civil-servant/console', async function (req, res, next) {
-  let ansify = new AU.default;
-  let stdout = await request({
+app.get('/civilservant/console', async function (req, res, next) {
+  const ansify = new AU.default()
+  const stdout = await request({
     url: 'https://files.chippy.ch/cs/stdout.txt'
   })
   let splat = stdout.trim().split('======')
@@ -48,28 +48,28 @@ app.get('/civil-servant/console', async function (req, res, next) {
   })
   return res.render('console.pug', {
     req,
-    stdout: splat//JSON.stringify(splat, null, 4)
+    stdout: splat// JSON.stringify(splat, null, 4)
   })
 })
 
-app.get('/civil-servant/notes', async function (req, res, next) {
-  let ansify = new AU.default;
-  let stdout = await request({
+app.get('/civilservant/notes', async function (req, res, next) {
+  const ansify = new AU.default()
+  const stdout = await request({
     url: 'https://files.chippy.ch/cs/notes.txt'
   })
-  let notes = stdout.trim().split('\n')
+  const notes = stdout.trim().split('\n')
   return res.render('notes.pug', {
     req,
     notes
   })
 })
 
-app.get('/civil-servant/scram', (req, res) => res.render('scram.pug', {req, scram}))
-app.post('/civil-servant/scram', (req, res) => {
+app.get('/civilservant/scram', (req, res) => res.render('scram.pug', { req, scram }))
+app.post('/civilservant/scram', (req, res) => {
   scram = {
     user: req.user,
     reason: req.body.reason
   }
-  return res.render('scram.pug', {req, scram})
+  return res.render('scram.pug', { req, scram })
 })
-app.get('/civil-servant/scram.json', (req, res) => res.json(scram))
+app.get('/civilservant/scram.json', (req, res) => res.json(scram))
